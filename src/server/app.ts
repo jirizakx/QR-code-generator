725a5247989d10802491bb20b0aa01b338ac5252
Jiri Zak <j@zak.software>
2026-02-12 22:59:20 +0100

    testing CI/CD

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,19 +1,22 @@
 import express from 'express';
+import { Router } from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
+const router = Router();
 const PORT = 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../views'));
 
-app.get('/', (_req, res) => {
+router.get('/', (_req, res) => {
   res.render('index', { title: 'Hello Jirka' });
 });
 
+app.use('/qr', router);
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
