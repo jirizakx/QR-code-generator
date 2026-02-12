7068bdc2e4f79b4e81b65c7f256689a41e0dfef1
Jiri Zak <j@zak.software>
2026-02-12 10:22:39 +0100

    basic project structure

---
+++ src/server/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
+
+app.get('/', (_req, res) => {
+  res.render('index', { title: 'Hello Jirka' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
